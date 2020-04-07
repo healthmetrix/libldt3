aa289e8faafb21942d8c42c54fa38b5ddf1ec004
Varun Jayaraman <varun.jayaraman@gmail.com>
2020-04-07 15:38:28 +0200

    Fix issues with loading ldt files

--- src/test/java/libldt3/integration/IntegrationTest.java
+++ src/test/java/libldt3/integration/IntegrationTest.java
@@ -1,12 +1,23 @@
 package libldt3.integration;
 import libldt3.LdtConstants;
 import libldt3.LdtReader;
+import libldt3.model.saetze.Satz;
+import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.*;
+import java.util.List;
 
 public class IntegrationTest {
     private final LdtReader reader = new LdtReader(LdtConstants.Mode.RELAXED);
+
     @Test
-    public void reads306Doc() {
-        String path = "3.0.6-normal.ldt";
+    public void reads306Doc() throws IOException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource("3.0.6-normal.ldt").getFile());
+
+
+        System.out.println(reader.read(file.toString()));
     }
 }
+ 
